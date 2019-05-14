42db33f83c2459d8b0fc4eb3e28fd254a40dd48b
gaellou <gaelle.vallet58@gmail.com>
2019-05-14 08:54:21 +0200

    Tours monstres et chemins .h

---
+++ monstre.h
@@ -0,0 +1,33 @@
+#ifndef __MONSTER_H__
+#define __MONSTER_H__
+
+
+#include "node.h"
+
+typedef struct s_monstre{
+    int x;
+    int y;
+    node * dest;
+    node * oldDest;
+    int vitesse;
+    int vie;
+    int direction; // orientation du sprite
+    int idmonstre; //orde dans la vague
+    int typemonstre;
+    float resistTour1;
+    float resistTour2;
+    float resistTour3;
+    float resistTour4;
+    int go;
+    struct s_monstre* next;
+} monstre;
+typedef monstre* l_monstre;
+
+l_monstre ajoutermonstre(l_monstre liste, l_node listeNode, int idmonstre, int vie, int typemonstre, float resistTour1,float resistTour2,float resistTour3,float resistTour4);
+l_monstre supprimermonstre(l_monstre liste, int idmonstre);
+l_monstre supprimerAllmonstre(l_monstre liste);
+void deplacemonstre (l_monstre liste, float timeVague, int vague, char** message, int * bouge, int * WinLose);
+void Affichermonstre (l_monstre liste, int vague, int * bouge);
+void DessinMonstre(l_monstre liste, int vague, int mouv);
+
+#endif
---
+++ tour.h
@@ -0,0 +1,31 @@
+#ifndef __TOWER_H__
+#define __TOWER_H__
+
+
+#include "monstre.h"
+
+typedef struct s_Tour{
+    int x; //position
+    int y;
+    int rayon;
+    int cost;
+    int type;
+    int puissance;
+    float cadence;
+    int active;
+    float timeCrea;
+    float timeConst;
+    float timeDernierTir;
+    struct s_Tour* next;
+} Tour;
+typedef Tour* l_Tour;
+
+l_Tour ajouterTour(l_Tour liste, int* coins, int x, int y,int type, char** message);
+l_Tour supprimerAllTour(l_Tour liste);
+l_Tour selectTour(l_Tour liste, int x, int y, char** message);
+void glisserTour(int x, int y, int type);
+l_Tour supprimerTour(l_Tour liste, l_Tour aSuppr);
+void afficherTour(l_Tour liste, int ray, l_Tour TourSelected);
+void gestionTour(l_Tour listeTour, l_monstre listemonstre);
+
+#endif
